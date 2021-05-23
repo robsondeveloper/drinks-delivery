d5521994c46ab59181b02b5f5beeb322ea240fa8
Robson Oliveira Da Costa <robsondeveloper@gmail.com>
2021-05-23 16:47:51 -0400

    cria projeto

---
+++ src/main/java/drinks/DrinksDeliveryApplication.java
@@ -0,0 +1,13 @@
+package drinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinksDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinksDeliveryApplication.class, args);
+	}
+
+}
